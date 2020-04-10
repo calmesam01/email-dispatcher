833e362a544ee8d8c4f05958f85c7d9896a8ec7f
calmesam01 <sanyamsareen01@gmail.com>
2020-04-09 23:43:41 -0400

    Add the email-dispatcher code

---
+++ routes/api.js
@@ -0,0 +1,50 @@
+var express = require('express')
+var router = express.Router()
+var utils = require('../utils')
+
+
+router.post('/:action', function(req, res, next) {
+
+	var action = req.params.action
+
+	if (action == 'send'){ // send an email
+		var recipients = req.body.recipients // comma separated list of emails
+		var list = recipients.split(',')
+
+		utils.Email.sendEmails(list, req.body, function(){
+		    res.json({
+		    	confirmation: 'success',
+		    	message: 'Emails Sent'
+		    })
+		})
+
+
+
+		// utils.Email
+		// .sendEmail(req.body)
+		// .then(function(response){
+		    // res.json({
+		    // 	confirmation: 'success',
+		    // 	response: response
+		    // })
+		// })
+		// .catch(function(err){
+		//     res.json({
+		//     	confirmation: 'fail',
+		//     	message: err
+		//     })
+		// })
+
+		return
+	}
+
+    res.json({
+    	confirmation: 'fail',
+    	message: 'Invalid Action'
+    })
+
+})
+
+
+
+module.exports = router
